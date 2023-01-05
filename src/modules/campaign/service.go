25cf072ff8b62e9aa6734305e974be70425280ea
Patra <patra@eigen.co.id>
2023-01-05 15:39:09 +0700

    feat: service campaign

--- src/modules/campaign/service.go
+++ src/modules/campaign/service.go
@@ -1,1 +1,23 @@
 package campaign
+
+type Service interface {
+	FindCampaigns(userID int) ([]Campaign, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repo Repository) *service {
+	return &service{
+		repo,
+	}
+}
+
+func (s *service) FindCampaigns(userID int) ([]Campaign, error) {
+	if userID != 0 {
+		return s.repository.FindByUserID(userID)
+	}
+
+	return s.repository.FindAll()
+}
